07756ba6a881ec4716aade1d30c2741537909f4b
Chirag <chiragshah4702@gmail.com>
2023-06-30 16:30:57 +0530

    Initial commit

--- src/main/java/com/sheriyans/major/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oops.major.repository;
-
-import com.oops.major.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
